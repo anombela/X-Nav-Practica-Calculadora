d0e54b0c4be270737a819755e295ae17507b1ee2
anombela <a.nombela93@hotmail.com>
2016-03-30 23:59:43 +0200

    hecho lo basico

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,40 @@
 jQuery(document).ready(function() {
+
+	var ans = "";
+
+	function limpiar(){
+
+		$("#pantalla").val("");
+	} 
+	limpiar();  //esto se ejecuta solo al inicio
+
     
+    $('.tecla').click(function(){
+
+    	var valor = $(this).text();
+    	
+    	if (valor == "AC"){
+    		limpiar();
+    		return;
+    	}
+    	else if (valor == "DEL"){
+    		$("#pantalla").val($("#pantalla").val().slice(0,$("#pantalla").val().length-1));
+    		return;
+    	}
+    	else if (valor == "ANS"){
+    		valor = ans;
+    	}
+
+    	$("#pantalla").val($("#pantalla").val() + valor);
+   
+    });
+
+
+    $('.igual').click(function(){
+    	var resultado = eval($('#pantalla').val());
+    	ans = resultado;
+    	$("#pantalla").val(resultado);
+    	console.log(resultado);
+    });
+
 });
